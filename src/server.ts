784568ae4b3cc524fee3b9b0afd68d4127cfa14c
SergioCarvajalV <scarvajal@utem.cl>
2022-08-04 06:50:59 -0400

    importacion de express, cors y compression

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("hola mundo");
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+app.use('/', (re,res) => {
+    res.send('Hola mundo');
+})
+
+const PORT = 5000
+app.listen(
+    { port: PORT},
+    ()=> console.log(`Hola mundo API GQL http://localhost:${PORT}`)
+);

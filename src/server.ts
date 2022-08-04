7140eabccfc629772414e707b5341d818eff7e03
SergioCarvajalV <scarvajal@utem.cl>
2022-08-04 07:37:29 -0400

    Hola mundo hecho con query en playground

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,63 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
-
+import { createServer } from 'http';
+import { GraphQLSchema } from 'graphql';
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-express';
+import expressPlayGround from "graphql-playground-middleware-express"
 const app = express();
 
 app.use(cors());
 app.use(compression());
-app.use('/', (re,res) => {
-    res.send('Hola mundo');
-})
 
-const PORT = 5000
+const typeDefs = `
+    type Query {
+        hola: String!
+        holaConNombre(nombre: String!): String!
+        numero:Int!
+    }
+`;
+
+const resolvers = {
+    Query:{
+        hola(): string{
+            return "HOLA MUNDO";
+        },
+        holaConNombre(root:object, args: { name:string}, context: object, info:object): string{
+            console.log(info);
+            return `Hola ${args.name}`;
+        },
+        numero(): number{
+            return 1;
+        }
+    }
+}
+
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+
+const server = new ApolloServer({
+    schema,
+    introspection: true,
+    playground: true
+});
+
+server.applyMiddleware({app});
+
+app.use('/hola', (_,res) => {
+    res.send('Hola mundo');
+});
+
+app.use('/', expressPlayGround({
+    endpoint: "/graphql"
+}));
+
+const PORT = 5000;
+
+const httpServer = createServer(app)
+
 app.listen(
     { port: PORT},
     ()=> console.log(`Hola mundo API GQL http://localhost:${PORT}`)
